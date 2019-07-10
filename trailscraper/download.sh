96680d54ca9579829deaff15469e98fc273ef268
James Michael DuPont <James.DuPont@nrg.com>
2019-07-10 14:53:00 +0000

    helpers

---
+++ trailscraper/download.sh
@@ -0,0 +1,1 @@
+trailscraper --verbose  download --bucket aws-nrgenergyprod-ct-all --account-id 877023909339 --region us-east-1 --profile prod  --from 'two days ago' --to 'now'
---
+++ trailscraper/generate_policies.sh
@@ -0,0 +1,9 @@
+for x in  /home/ec2-user/.trailscraper/logs/AWSLogs/877023909339/CloudTrail/us-east-1/2019/*/*/*.json.gz;
+do
+    if [ ! -f $x.policy ]; then
+	echo $x;
+	zcat $x | trailscraper generate > $x.policy;
+    else
+	echo $x.policy exists
+    fi
+done
